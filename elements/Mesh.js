fcb1b029d1d273ca6232902f3ff1b985b88aca15
facobackup <gustavomicaelbarbosa@gmail.com>
2021-12-18 21:58:02 -0300

    SUS

--- elements/Mesh.js
+++ elements/Mesh.js
@@ -29,7 +29,7 @@ export default class Mesh {
             updatedC = scaleIntoView(projectVector(MatrixMultiplyVector(viewMatrix, tri.vectors[2].matrix), fieldOfView, aspectRatio, zScale, zOffset), canvasWidth, canvasHeight)
         let currentTriangle = new Triangle([updatedA[0][0], updatedA[1][0], updatedA[2][0]], [updatedB[0][0], updatedB[1][0], updatedB[2][0]], [updatedC[0][0], updatedC[1][0], updatedC[2][0]])
 
-        currentTriangle.color = `hsl(0, 10%, ${dotProdLightVec <= 0 ?' 10%' : (dotProdLightVec * 50)}%)`
+        currentTriangle.color = `hsl(0, 10%, ${dotProdLightVec <= 0 ? ' 10%' : (dotProdLightVec * 50)}%)`
 
         return currentTriangle
     }
@@ -77,12 +77,12 @@ export default class Mesh {
     draw(ctx, engineAttrs, debug) {
         const sortTriangles = (triangleA, triangleB) => {
             let z1 = Math.abs(triangleA.vectors[0].z + triangleA.vectors[1].z + triangleA.vectors[2].z) / 3
-            let z2 =  Math.abs((triangleB.vectors[0].z + triangleB.vectors[1].z + triangleB.vectors[2].z))/ 3
+            let z2 = Math.abs((triangleB.vectors[0].z + triangleB.vectors[1].z + triangleB.vectors[2].z)) / 3
 
-            if ( z1 < z2 ){
+            if (z1 < z2) {
                 return -1;
             }
-            if ( z2 > z1 ){
+            if (z2 > z1) {
                 return 1;
             }
             return 0;
@@ -92,7 +92,61 @@ export default class Mesh {
 
         toRaster = toRaster.sort(sortTriangles)
 
-        for (let current = 0; current < toRaster.length; current++)
-            toRaster[current].draw(ctx, debug)
+        for (let current = 0; current < toRaster.length; current++) {
+            let listTriangles = []
+
+            listTriangles.push(toRaster[current]);
+            let nNewTriangles = 1;
+
+            for (let p = 0; p < 4; p++) {
+                let nTrisToAdd = 0;
+                while (nNewTriangles > 0) {
+
+                    let test = listTriangles[listTriangles.length - 1];
+                    listTriangles.pop();
+                    nNewTriangles--;
+                    switch (p) {
+                        case 0: {
+                            nTrisToAdd = clipAgainstPlane(
+                                [0, 0, 0],
+                                [0, 1, 0],
+                                test)
+                            break
+                        }
+                        case 1: {
+                            nTrisToAdd = clipAgainstPlane(
+                                [1, ctx.canvas.height - 1, 0],
+                                [0, -1, 0],
+                                test)
+
+                            break
+                        }
+                        case 2: {
+                            nTrisToAdd = clipAgainstPlane(
+                                [0, 0, 0],
+                                [1, 0, 0],
+                                test)
+                            break
+                        }
+                        case 3: {
+                            nTrisToAdd = clipAgainstPlane(
+                                [ctx.canvas.width - 1, 0, 0],
+                                [-1, 0, 0],
+                                test
+                            )
+                            break
+                        }
+                    }
+
+                    for (let w = 0; w < nTrisToAdd.quantity; w++)
+                        listTriangles.push(nTrisToAdd.triangles[w]);
+                }
+                nNewTriangles = listTriangles.length;
+            }
+
+            listTriangles.forEach(tri => {
+                tri.draw(ctx, debug)
+            })
+        }
     }
 }
